19f53f16b4242209ff8acf7d17fd8d0a08e35f05
Akanksha <nakkaakanksha97@gmail.com>
2023-12-19 07:13:37 +0000

    solution

---
+++ src/main/java/com/example/geohub/repository/CityRepository.java
@@ -0,0 +1,19 @@
+package com.example.geohub.repository;
+
+import java.util.ArrayList;
+import com.example.geohub.model.City;
+import com.example.geohub.model.Country;
+
+public interface CityRepository {
+    ArrayList<City> getCities();
+
+    City getCityById(int cityId);
+
+    City addCity(City city);
+
+    City updateCity(int cityId, City city);
+
+    void deleteCity(int cityId);
+
+    Country getCityCountry(int cityId);
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,46 @@
+insert into country(countryName,currency,population,latitude,longitude)
+values('India','INR',1393409038,'20.5937° N','78.9629° E');
+
+insert into country(countryName,currency,population,latitude,longitude)
+values('USA','USD',784487464,'20.5937° N','78.9629° E');
+
+insert into country(countryName,currency,population,latitude,longitude)
+values('Australia','AUD',896474747,'20.5937° N','78.9629° E');
+
+insert into country(countryName,currency,population,latitude,longitude)
+values('Canada','CAD',784644849,'20.5937° N','78.9629° E');
+
+insert into country(countryName,currency,population,latitude,longitude)
+values('UK','GBP',86474844,'20.5937° N','78.9629° E');
+
+insert into city(cityName, population, latitude, longitude, countryId)
+values('Mumbai', 7833368, '19.0760 N', '72.8777 E', 1);
+
+insert into city(cityName, population, latitude, longitude, countryId)
+values('Bangalore', 7845633, '12.9716 N', '77.5946 E', 1);
+
+insert into city(cityName, population, latitude, longitude, countryId)
+values('New York', 7853436, '40.7128 N', '74.0060 W', 2);
+
+insert into city(cityName, population, latitude, longitude, countryId)
+values('Chicago', 56777484, '41.8781 N', '87.6298 W', 2);
+
+insert into city(cityName, population, latitude, longitude, countryId)
+values('Sydney', 5634353, '33.8688 S', '151.2093 E', 3);
+
+insert into city(cityName, population, latitude, longitude, countryId)
+values('Melbourne', 56373637, '37.8136 S', '144.9631 E', 3);
+
+insert into city(cityName, population, latitude, longitude, countryId)
+values('Vancouver', 67544556, '49.2827 N', '123.1207 W', 4);
+
+insert into city(cityName, population, latitude, longitude, countryId)
+values('Toronto', 45678883, '43.6532 N', '79.3832 W', 4);
+
+insert into city(cityName, population, latitude, longitude, countryId)
+values('London', 89754647, '51.5074 N', '0.1278 W', 5);
+
+insert into city(cityName, population, latitude, longitude, countryId)
+values('Manchester', 5634535, '53.4830 N', '2.2444 W', 5);
+
+
